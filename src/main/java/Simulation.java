5ef6a0c85463c7d863d581e189b6aa7546362d98
michal_skiba_2000 <michal.skiba.2000@gmail.com>
2020-05-12 14:29:42 +0200

    Simulation class init

--- src/main/java/Simulation.java
+++ src/main/java/Simulation.java
@@ -1,29 +1,89 @@
 import java.util.List;
 import java.time.LocalTime;
 
+/**
+ * Main class that handles the simulation
+ */
 public class Simulation {
+     // Musimy wpierw zaimplementować klasy Tram, Sector, RandomEvent
+     // zanim zaczniemy implementację przeprowadzenia symulacji
+
     // private List<Tram> all_trams = new ArrayList<Tram>();
     // private List<Sector> all_sectors = new ArrayList<Sector>();
     // private List<RandomEvent> all_possible_events = new ArrayList<RandomEvent>();
     private LocalTime time;
 
-    public void spawn_passengers(){
-        System.out.println("spawn");
+    /**
+     * Set sectors based on prepared map
+     */
+    private void set_sectors(){
+        System.out.println("set sectors");
     }
 
-    public void resolve_random_events(){
-        System.out.println("resolve");
+    /**
+     * Set tramlines based on prepared schedule
+     */
+    private void set_tramlines(){
+        System.out.println("set tramlines");
     }
 
-    public void move_trams(){
-        System.out.println("move");
+    /**
+     * Spawn trams based on tramlines
+     */
+    private void spawn_trams(){
+        System.out.println("spawn trams");
     }
 
-    public Simulation(){
-        // Set simualtion parameters
+    /**
+     * Resolve random events from all_possible_events list
+     * and if any happen set event to tram or sector
+     */
+    private void resolve_random_events(){
+        System.out.println("resolve random events");
+    }
+
+    /**
+     * Spawn passengers on trams based on time
+     */
+    private void spawn_passengers() {
+        System.out.println("spawn passengers");
+    }
+
+    /**
+     * Resolve move function on all trams
+     */
+    private void move_trams(){
+        System.out.println("move trams");
+    }
+
+    /**
+     * Repeat actions for one turn,
+     * random events, spawning passengers, moving trams
+     * until 23:00
+     */
+    private void simulate(){
+        // potrzebujemy jakichś struktur w których będziemy zapisywać dane symulacji
+        while( !(time.getHour() == 23 && time.getMinute() == 0) ){
+            resolve_random_events();
+            spawn_passengers();
+            move_trams();
+            time = time.plusMinutes(1);
+        }
+    }
+
+    /**
+     * Set default parameters for simulation
+     * Start simulating
+     */
+    private Simulation(){
+        time = LocalTime.of(5, 0, 0);
+        set_sectors();
+        set_tramlines();
+        spawn_trams();
+        simulate();
     }
 
     public static void main(String[] args){
-        // Start simulation
+        Simulation simulation = new Simulation();
     }
 }
